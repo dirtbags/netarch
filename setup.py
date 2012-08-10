ca5ca05b1bbaaa021705ec3bb48a0023f99ea9a1
pi-rho <pi-rho@tyr.cx>
2012-08-09 20:24:56 -0500

    House cleaning; moved to version 1.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name="netarch",
-    version="0.0",
+    version="1.0",
     author="Neale Pickett",
     author_email="neale@lanl.gov",
     description=("Classes to aid in the dissection of network protocols and cryptanalysis"),
-    long_description=open("README.txt").read(),
-    license=open("COPYING.txt").read(),
+    long_description=open("README").read(),
+    license=open("COPYING").read(),
     keywords="network protocol dissection cryptanalysis",
     url="http://woozle.org/~neale/projects/netarch",
     classifiers=[
