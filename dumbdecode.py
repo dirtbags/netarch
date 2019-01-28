6cf9614b9700852a9a81cf40644d5c1922b85ff0
Neale Pickett <neale@lanl.gov>
2019-01-28 17:15:42 +0000

    make dumbdecode a little less dumb and a little more helpful

--- dumbdecode.py
+++ dumbdecode.py
@@ -4,10 +4,17 @@ import sys
 from netarch import ip
 from netarch import *
 
+class DumbPacket(ip.Packet):
+    def parse(self, data):
+        self.payload = data
+
+class DumbSession(ip.Session):
+    Packet = DumbPacket
+
 s = None
 reseq = ip.Dispatch(*sys.argv[1:])
 for h, d in reseq:
     srv, first, chunk = d
     if not s:
-        s = ip.Session(first)
+        s = DumbSession(first)
     s.handle(srv, first, chunk, reseq.last)
